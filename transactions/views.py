5860ed9e7dcfea2d4e8a571963a42edb6dec5908
GibsonLyrio <159889118+GibsonLyrio@users.noreply.github.com>
2024-08-06 05:44:43 -0300

    All CRUD functions configured
    Next steps:
    - Improve the update function because has some weird bugs;
    - Implementation of some login and users logic

--- transactions/views.py
+++ transactions/views.py
@@ -5,29 +5,43 @@ from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 
 # Create your views here.
 def index(request):
-  result = Transactions.objects.all()
-  return render(request, 'index.html', { 'transactions': result })
+  context = {}
+  context['transactions'] = Transactions.objects.all()
+  if Transactions.objects.count() == 0:
+    return render(request, 'first.html', context)
+  return render(request, 'index.html', context)
 
 @csrf_protect
 def form(request):
+  context = {}
   if request.method == 'GET':
-    return render(request, 'form.html')
+    return render(request, 'form.html', context)
 
   if request.method == 'POST':
-    newTransactions = Transactions()
+    print(f'\033[33m some values{request.POST}\033[m')
+    if 'delete' in request.POST:
+      id = request.POST.get('delete')
+      transaction = Transactions.objects.get(id=id)
+      transaction.delete()
+      return HttpResponseRedirect('/transactions')
     
-    if request.POST.get('type') == 'receita':
-      newTransactions.type = True
-    if request.POST.get('type') == 'despesa':
-      newTransactions.type = False
+    if 'edit' in request.POST:
+      id = request.POST.get('edit')
+      transaction = Transactions.objects.get(id=id)
+      context['editMode'] = True
+      context['editFormData'] = transaction
+      transaction.delete()
+      return render(request, 'form.html', context)
 
+    newTransactions = Transactions()
+
+    newTransactions.type = request.POST.get('type')
     newTransactions.value = request.POST.get('value')
     newTransactions.description = request.POST.get('description')
+
     newTransactions.save()
     
     return HttpResponseRedirect('/transactions')
-  
-  if request.method == 'PUT':
-    print('PUT method')
-    print(request.PUT.get('type'))
-    return HttpResponse.status_code(200)
+
+def first(request):
+  return render(request, 'first', context={})

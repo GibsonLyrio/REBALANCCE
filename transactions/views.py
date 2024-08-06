45c95d97450ca8d96a37ec3457733eea4b54b835
GibsonLyrio <159889118+GibsonLyrio@users.noreply.github.com>
2024-08-05 23:52:50 -0300

    Small corrections & stylization

--- transactions/views.py
+++ transactions/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect 
 from .models import Transactions
+from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 
 # Create your views here.
 def index(request):
   result = Transactions.objects.all()
   return render(request, 'index.html', { 'transactions': result })
 
+@csrf_protect
 def form(request):
-  return render(request, 'form.html')
+  if request.method == 'GET':
+    return render(request, 'form.html')
+
+  if request.method == 'POST':
+    newTransactions = Transactions()
+    
+    if request.POST.get('type') == 'receita':
+      newTransactions.type = True
+    if request.POST.get('type') == 'despesa':
+      newTransactions.type = False
+
+    newTransactions.value = request.POST.get('value')
+    newTransactions.description = request.POST.get('description')
+    newTransactions.save()
+    
+    return HttpResponseRedirect('/transactions')
+  
+  if request.method == 'PUT':
+    print('PUT method')
+    print(request.PUT.get('type'))
+    return HttpResponse.status_code(200)

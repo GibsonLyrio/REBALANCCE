c0dc57f358b95b3c4998b60bf042228d6029d339
GibsonLyrio <159889118+GibsonLyrio@users.noreply.github.com>
2024-08-07 23:51:05 -0300

    Basic implementation of home screen

--- transactions/views.py
+++ transactions/views.py
@@ -1,83 +1,150 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_protect 
+from django.views.decorators.csrf import csrf_protect
 from .models import Transactions
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 from django.urls import reverse
 
-### Create your views here.
-# index page handler:
+# index page handler
 def index(request):
-  if request.method != 'GET':
-    # early return, method no allowed
-    return HttpResponse(status_code=405)
-  
   context = {}
-  context['transactions'] = Transactions.objects.all()
 
-  # get all in database, and if isn't have any, redirect do first form:
+  # early return, method no allowed
+  if request.method != 'GET':
+    return HttpResponse(status_code=405)
+
+  # tries get transactions of database
+  try:
+    context['transactions'] = Transactions.objects.all()
+    print(f'\033[34mTransações encontradas = [\n {context['transactions']}\n]\033[m')
+    context['getAllTransactionsSuccess'] = True
+  except:
+    # if get operation failed
+    context['getAllTransactionsSuccess'] = False
+    return HttpResponse('<H1>Não foi possível encontrar as transações</H1>')
+
+  # if isn't have any, set first transaction flag
   if context['transactions'].count() == 0:
+    context['firstTransaction'] = True
     return render(request, 'first.html', context)
+  context['firstTransaction'] = False
 
-  # otherwise, return de index page with all transactions: 
+  # return de index page with all transactions
   return render(request, 'index.html', context)
 # ----
 
-# first form page handler:
+# first form page handler
+# TODO: remove this method and add in the index page the handler of this function of 'first transaction' warning
+@csrf_protect
 def first(request):
   context = {}
+
+  # early return, method no allowed
+  if request.method != 'GET':
+    return HttpResponse(status_code=405)
+
   return render(request, 'first.html', context)
 # ----
 
 # transaction form page handler:
+@csrf_protect
 def form(request):
   context = {}
+
+  # early return, method no allowed
+  if request.method != 'GET':
+    return HttpResponse(status_code=405)
+
   return render(request, 'form.html', context)
 # ----
 
-# add transaction backend handler:
+# add transaction backend handler
+@csrf_protect
 def formAdd(request):
   context = {}
-  context['transaction'] = request.POST
 
-  transaction = Transactions()
-  transaction.type = context['transaction'].get('type')
-  transaction.value = context['transaction'].get('value')
-  transaction.description = context['transaction'].get('description')
-  transaction.save()
-  
+  # early return, method no allowed
+  if request.method != 'POST':
+    return HttpResponse(status_code=405)
+
+  context['transaction'] = request.POST   # get request info
+
+  # tries create a new transaction and save in DB
+  try:
+    transaction = Transactions()  # new instance of Transactions
+    # setting the values fields of new transaction
+    transaction.type = context['transaction'].get('type')
+    transaction.value = context['transaction'].get('value')
+    transaction.description = context['transaction'].get('description')
+    # save this new transaction with infos
+    transaction.save()
+    context['addTransactionSuccess'] = True
+  except:
+    context['addTransactionSuccess'] = False
+    return HttpResponse('<H1>Não foi possível salvar transação</H1>')
+
   return HttpResponseRedirect(reverse('index'))
 # ----
 
-#  edit transaction backend handler:
+#  edit transaction backend handler
+@csrf_protect
 def formEdit(request):
   context = {}
-  context['transaction'] = request.POST
 
+  # early returnn method not allowed
+  if request.method != 'POST':
+    return HttpResponse(status=405) 
+
+  context['transaction'] = request.POST   # get request info
+
+  # verify which operation need be do
   if 'update' in request.POST:
     id = request.POST.get('update')
-    transaction = Transactions.objects.get(id=id)
+    # tries get transaction by id to be updated
+    try:
+      transaction = Transactions.objects.get(id=id)
+      context['getTransactionSuccess'] = True
+    except:
+      context['getTransactionSuccess'] = False
+      return HttpResponse('<H1>Não foi possível encontrar a transação selecionada</H1>')
     context['transaction'] = transaction
     context['editMode'] = True
     return render(request, 'form.html', context)
 
   if 'delete' in request.POST:
     id = request.POST.get('delete')
-    transaction = Transactions.objects.get(id=id)
-    transaction.delete()
+    try:
+      transaction = Transactions.objects.get(id=id)
+      transaction.delete()
+      context['getTransactionSuccess'] = True
+    except:
+      context['getTransactionSuccess'] = False
+      return HttpResponse('<H1>Não foi possível encontrar a transação selecionada</H1>')
 
   return HttpResponseRedirect(reverse('index'))
 # ---
 
-# update transaction data backend function:
+# update transaction data backend function
+@csrf_protect
 def updateTransaction(request):
   context = {}
-  context['transaction'] = request.POST
 
-  transaction = Transactions.objects.get(id=context['transaction'].get('id'))
-  transaction.type = context['transaction'].get('type')
-  transaction.value = context['transaction'].get('value')
-  transaction.description = context['transaction'].get('description')
-  transaction.save()
+  # early return, method no allowed
+  if request.method != 'POST':
+    return HttpResponse(status_code=405)
+  
+  context['transaction'] = request.POST   # get request info
+
+  # tries update the transaction in DB
+  try:
+    transaction = Transactions.objects.get(id=context['transaction'].get('id'))
+    transaction.type = context['transaction'].get('type')
+    transaction.value = context['transaction'].get('value')
+    transaction.description = context['transaction'].get('description')
+    transaction.save()
+    context['updateTransactionSuccess'] = True
+  except:
+    context['updateTransactionSuccess'] = False
+    return HttpResponse('Não foi possível atualizar a transação')  
 
   return HttpResponseRedirect(reverse('index'))
 # ----
